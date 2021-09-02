54bf42965e88f49e4597dc62ea8bface0af52651
João Vítor <joaovsoutosantos@gmail.com>
2021-09-01 22:58:42 -0300

    Add styled components and configure initial theme

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,13 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    yellow: {
+      500: string;
+    };
+
+    gray: {
+      800: string;
+    };
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+  yellow: {
+    500: '#face48',
+  },
+
+  gray: {
+    800: '#2d2d2d',
+  },
+};
