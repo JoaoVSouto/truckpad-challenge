3db6ef5d362c2664654e5433313ca2b3d14234da
João Vítor <joaovsoutosantos@gmail.com>
2021-09-02 09:56:48 -0300

    Create Home initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 
+import { Home } from 'pages/Home';
+
 import GlobalStyles from 'styles/global';
 import light from 'styles/themes/light';
 
 export default function App() {
   return (
     <ThemeProvider theme={light}>
-      <h1>hello world</h1>
+      <Home />
 
       <GlobalStyles />
     </ThemeProvider>
